acec117bbe1f9e16547bd9bb44bda8e9b3bbee34
Mike Yumatov <mike@yumatov.org>
2012-11-08 00:28:35 +0400

    Add current file's dir to LESS search paths list

--- gears_less/compiler.py
+++ gears_less/compiler.py
@@ -7,3 +7,12 @@ class LESSCompiler(ExecCompiler):
     result_mimetype = 'text/css'
     executable = 'node'
     params = [os.path.join(os.path.dirname(__file__), 'compiler.js')]
+
+    def __call__(self, asset):
+        self.asset = asset
+        super(LESSCompiler, self).__call__(asset)
+
+    def get_args(self):
+        args = super(LESSCompiler, self).get_args()
+        args.append(self.asset.absolute_path)
+        return args
